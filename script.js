2174deaee2a22f2fd47dcb8fd25d91e8f4fdc0bf
MrAshwin2142 <89156541+MrAshwin2142@users.noreply.github.com>
2023-03-06 22:47:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,5 @@ box.style.backgroundColor="navy";
 let clr=['yello','green','purple','red','black','blue','orange','pink','gray','olive'];
 btn.addEventListener('mouseenter',(()=>{
     let num=Math.floor(Math.random() * 10);
-    box.style.backgroundColor=clr[num];
-    btn.style.opacity=0;
-    future.innerHTML="your future in very bright"   
+    box.style.backgroundColor=clr[num];   
 }))
